72d145859bd99d69d9a5b774edd31f06c70eecc5
fumakama <77727552+fumakama@users.noreply.github.com>
2023-06-23 14:52:32 +0400

    Add grafical interface to rock-paper-scissors

--- script.js
+++ script.js
@@ -6,9 +6,9 @@ function getComputerChoice() {
     return results[randomOfThree];
 }
 
-// Return the result of one round of the game
+// Function returns the result of one round of the game
 
-function playRound(playerSelection, computerSelection) {
+function getRoundResult(playerSelection, computerSelection) {
     let result = (playerSelection === "rock" && 
         computerSelection === "scissors") ? "You Win! Rock beats Scissors" :
                  (playerSelection === "rock" &&
@@ -25,35 +25,80 @@ function playRound(playerSelection, computerSelection) {
     return result;
 }
 
+// Function Updates the page after each played round of the game
+function playRound(playerSelection, score) {
+    // Get computer choice
+    let computerSelection = getComputerChoice();
+    // Get the elements of the page
+    const para = document.querySelector('.paraResult');
+    const overallScore = document.querySelector('.overall');
+    // Get result of the round and update the page content
+    let roundResult = getRoundResult(playerSelection, computerSelection);
+    para.textContent = roundResult;
+    // Update score and print it
+    score = updateScore(roundResult, score);
+    overallScore.textContent = `Player - ${score.player} : Computer - ${score.computer}`;
+
+    // Print out the result of the round and the whole score after it to the console
+    console.log(roundResult);
+    console.log(`Player - ${score.player} : Computer - ${score.computer}`);
+}
+
+
+
+// Function updates the score 
+function updateScore(roundResult, score) {
+    scoreMarker = roundResult.substring(0, 5);
+    if (scoreMarker === "You W") {
+        score.player++;
+    } else if (scoreMarker === "You L") {
+        score.computer++;
+    } 
+    return score;
+}
+// Function checks if it is a gameover
+function checkGameover(score) {
+    let gameover = document.querySelector('.gameover');
+    if (score.player === 5) {
+        // Output for player winner
+        gameover.textContent = 'Game Over! Player won the Match!';
+        
+    } else if (score.computer === 5) {
+        // Output for computer winner 
+        gameover.textContent = 'Game Over! Computer won the Match!';
+    }
+}
+
 // Main Game function
 function game() {
-
-    let pScore = 0;
-    let cScore = 0;
-
-    for (let i = 0; i < 5; i++) {
-
-        computerSelection = getComputerChoice();
-        
-        // Here the loop prompts user untill he makes a valid choice
-        do {
-            playerSelection = prompt("Rock, Paper or Scissors?").toLowerCase();
-        } while (playerSelection != "rock" && playerSelection != "paper" && playerSelection != "scissors");
-
-        // Get the resulting string of the game
-        let roundResult = playRound(playerSelection, computerSelection);
-        // Check and change the score of each player
-        scoreMarker = roundResult.substring(0, 5);
-        if (scoreMarker === "You W") {
-            pScore++;
-        } else if (scoreMarker === "You L") {
-            cScore++;
-        } 
-
-        // Print out the result of the round and the whole score after it
-        console.log(roundResult);
-        console.log(`Player - ${pScore} : Computer - ${cScore}`);
+    
+    // Declare the score object
+    let score = {
+        player: 0,
+        computer: 0
     }
+
+    // Get the content of .gameover paragraph
+    let gameover = document.querySelector('.gameover');
+    gameover.textContent = '';
+
+    // Add the logic to 3 buttons with three different variants
+    const btnRock = document.querySelector('#rock');
+    btnRock.addEventListener('click', () => {
+        playRound('rock', score);
+        checkGameover(score);
+    });
+
+    const btnPaper = document.querySelector('#paper');
+    btnPaper.addEventListener('click', () => {
+        playRound('paper', score);
+        checkGameover(score);
+    });
+    const btnScissors = document.querySelector('#scissors');
+    btnScissors.addEventListener('click', () => {
+        playRound('scissors', score);
+        checkGameover(score);
+    });
 }
 
 // Call the main function after loading the page
